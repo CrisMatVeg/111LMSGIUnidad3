056d8c1367e70f5ef26cffa3403089decc728d70
Cristian Mateos <cristian.matveg@educa.jcyl.es>
2025-03-26 13:07:07 +0100

    Ejercicio 2 hecho

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,13 +1,13 @@
 var titulo=document.getElementById("titulo")
 titulo.innerText="DOM Modificado"
 var parrafos = document.getElementsByClassName("parrafo")
-parrafos.length
 parrafos[0].innerText="Parrafo 1 modificado"
 parrafos[1].innerHTML="<b>Parrafo 2</b> modificado"
 parrafos[2].innerText="Parrafo 3 modificado"
 //parrafos[0].outerHTML="<div>Parrafo 11 modificado</div>"
 document.getElementById("cuerpo").style.background="#f4f4f4"
 document.getElementById("cuerpo").style.textAlign="center"
+
 titulo.style.accentColor="#333"
 titulo.style.marginBottom="15px"
 titulo.style.textTransform="uppercase"
@@ -25,13 +25,15 @@ parrafos[2].style.background="#e6f2ff"
 parrafos[2].style.color="#3498db"
 parrafos[2].style.borderColor="#2980b9"
 
-for(i=0;i<=parrafos.length;i++){
+for(var i=0;i<=parrafos.length;i++){
     parrafos[i].style.padding="10px"
     parrafos[i].style.fontSize="18px"
     parrafos[i].style.borderLeftWidth="5px"
     parrafos[i].style.borderLeftStyle="solid"
     parrafos[i].style.borderRadius="5px"
     parrafos[i].style.lineHeight="1.6"
+    parrafos[i].style.margin="0 auto 15px"
+    parrafos[i].style.width="60%"
 }
 
 
